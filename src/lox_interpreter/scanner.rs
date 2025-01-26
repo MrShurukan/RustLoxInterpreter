ae947982568ca336c5953dd6b58c6796f96bf78a
Ilya Zavyalov <ilya.zavyalov0@gmail.com>
2025-01-27 00:06:56 +0300

    Сканирование литерала строк

--- src/lox_interpreter/scanner.rs
+++ src/lox_interpreter/scanner.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
+﻿use std::error::Error;
 use std::fmt::{format, Display, Formatter};
 use anyhow::bail;
 use crate::lox_interpreter::token::Token;
-use crate::lox_interpreter::token_type::{PunctuationType, TokenType};
+use crate::lox_interpreter::token_type::{LiteralType, PunctuationType, TokenType};
 
 pub struct Scanner<'a> {
     source: &'a str,
@@ -45,33 +45,33 @@ impl<'a> Scanner<'a> {
 
         match c {
             // Single char punctuation
-            '(' => self.add_token(P(PT::LeftParen)),
-            ')' => self.add_token(P(PT::RightParen)),
-            '{' => self.add_token(P(PT::LeftBrace)),
-            '}' => self.add_token(P(PT::RightBrace)),
-            ',' => self.add_token(P(PT::Comma)),
-            '.' => self.add_token(P(PT::Dot)),
-            '-' => self.add_token(P(PT::Minus)),
-            '+' => self.add_token(P(PT::Plus)),
-            ';' => self.add_token(P(PT::Semicolon)),
-            '*' => self.add_token(P(PT::Star)),
+            '(' => self.add_token_type(P(PT::LeftParen)),
+            ')' => self.add_token_type(P(PT::RightParen)),
+            '{' => self.add_token_type(P(PT::LeftBrace)),
+            '}' => self.add_token_type(P(PT::RightBrace)),
+            ',' => self.add_token_type(P(PT::Comma)),
+            '.' => self.add_token_type(P(PT::Dot)),
+            '-' => self.add_token_type(P(PT::Minus)),
+            '+' => self.add_token_type(P(PT::Plus)),
+            ';' => self.add_token_type(P(PT::Semicolon)),
+            '*' => self.add_token_type(P(PT::Star)),
 
             // Check for multichar punctuation (by checking the next char)
             '!' => {
                 let token_type = self.next_char_check('=', P(PT::BangEqual), P(PT::Bang));
-                self.add_token(token_type);
+                self.add_token_type(token_type);
             },
             '=' => {
                 let token_type = self.next_char_check('=', P(PT::EqualEqual), P(PT::Equal));
-                self.add_token(token_type);
+                self.add_token_type(token_type);
             },
             '<' => {
                 let token_type = self.next_char_check('=', P(PT::LessEqual), P(PT::Less));
-                self.add_token(token_type);
+                self.add_token_type(token_type);
             },
             '>' => {
                 let token_type = self.next_char_check('=', P(PT::GreaterEqual), P(PT::Greater));
-                self.add_token(token_type);
+                self.add_token_type(token_type);
             },
 
             // A special case: '/', as it's division and comments
@@ -85,7 +85,7 @@ impl<'a> Scanner<'a> {
                     }
                 }
                 else {
-                    self.add_token(P(PT::Slash));
+                    self.add_token_type(P(PT::Slash));
                 }
             },
 
@@ -94,18 +94,63 @@ impl<'a> Scanner<'a> {
             '\r' => { },
             '\t' => { },
 
-            '\n' => { self.line += 1; },
+            '\n' => self.line += 1,
 
+            // Literals
+            '"' => self.parse_string_literal()?,
 
             _ => return Err(
                 ScannerError::new_with_location(
                     ScannerErrorType::UnknownToken(String::from(c)),
                     self.line,
-                    self.get_line_with_marker(self.current).expect("Couldn't produce line with a marker")
+                    self.current,
+                    self.source
                 )
             )
         };
 
+        Ok(())
+    }
+
+    fn parse_string_literal(&mut self) -> Result<(), ScannerError> {
+        let start_string_index = self.current;
+        while let Some(current_char) = self.peek_current() {
+            match current_char {
+                '\n' => self.line += 1,
+                '"' => break,
+                _ => {}
+            }
+
+            self.advance();
+        }
+
+        if self.is_at_end() {
+            return Err(
+                ScannerError::new_with_location(
+                    ScannerErrorType::UnterminatedString,
+                    self.line,
+                    start_string_index,
+                    self.source
+                )
+            );
+        }
+
+        // Skip the closing quote
+        self.advance();
+
+        // Get the string value
+        let str = self.source.chars()
+            .skip(self.start + 1)
+            .take(self.current - self.start - 2)
+            .collect();
+        
+        let token = Token {
+            lexeme: format!("\"{str}\""),
+            token_type: TokenType::Literal(LiteralType::String(str)),
+            line: self.line
+        };
+        
+        self.add_token(token);
         Ok(())
     }
 
@@ -132,10 +177,14 @@ impl<'a> Scanner<'a> {
         true
     }
 
-    fn add_token(&mut self, token_type: TokenType) {
+    fn add_token_type(&mut self, token_type: TokenType) {
         let take_count = self.current - self.start;
         let lexeme = self.source.chars().skip(self.start).take(take_count).collect();
         self.tokens.push(Token { token_type, lexeme, line: self.line });
+    }
+    
+    fn add_token(&mut self, token: Token) {
+        self.tokens.push(token);
     }
 
     fn advance(&mut self) -> Option<char> {
@@ -159,11 +208,11 @@ impl<'a> Scanner<'a> {
     /// ### Arguments
     /// * `marker_position` - Absolute index of a char into the `self.source` string.
     /// It is computed by iterating over `self.source.chars()`
-    fn get_line_with_marker(&self, marker_position: usize) -> anyhow::Result<String> {
+    fn get_line_with_marker(source: &str, marker_position: usize) -> anyhow::Result<String> {
         let mut current_position: usize = 0;
         let mut line_start_position: usize = 0;
         let mut line_number: u32 = 1;
-        let mut chars = self.source.chars();
+        let mut chars = source.chars();
 
         // Find the line start
         while current_position != marker_position {
@@ -177,19 +226,19 @@ impl<'a> Scanner<'a> {
                     }
                 }
             }
-            
+
             current_position += 1;
         }
-        
-        let line: String = self.source.chars()
+
+        let line: String = source.chars()
             .skip(line_start_position)
             .take_while(|c| *c != '\n')
             .collect();
-        
+
         let marker_offset = marker_position - line_start_position;
         let line_number_string = format!("{line_number}. ");
         let spaces = " ".repeat(line_number_string.len() + marker_offset - 1);
-        
+
         Ok(format!("{line_number_string}{line}\n{spaces}^"))
     }
 
@@ -214,27 +263,32 @@ impl ScannerError {
         ScannerError { error_type, line, location: String::from("[not specified]") }
     }
 
-    pub fn new_with_location(error_type: ScannerErrorType, line: u32, location: String) -> Self {
+    pub fn new_with_location(error_type: ScannerErrorType, line: u32, marker_position: usize, source: &str) -> Self {
+        let location = Scanner::get_line_with_marker(source, marker_position)
+            .expect("Couldn't produce line with a marker");
         ScannerError { error_type, line, location }
     }
 }
 
 #[derive(Debug)]
 pub enum ScannerErrorType {
-    UnknownToken(String)
+    UnknownToken(String),
+    UnterminatedString
 }
 
 impl Error for ScannerError {}
 impl Display for ScannerError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let error_header =
-            format!("[line {}] Error @{}", self.line, self.location);
+            format!("[line {}] Error", self.line);
 
         let output = match &self.error_type {
             ScannerErrorType::UnknownToken(lexeme) => format!("Unknown token ('{}')", lexeme),
+            ScannerErrorType::UnterminatedString => "Unterminated string".parse().unwrap(),
+
             _ => format!("{:?}", self)
         };
 
-        write!(f, "{error_header}: {}", output)
+        write!(f, "{error_header}: {}\n\nHappened here:\n{}", output, self.location)
     }
 }
